eb30df31a35e43a46e815ee35c5c5579492eb8b9
ajay <ajaykumar290904@gmail.com>
2025-08-05 23:59:04 +0530

    feat: implement basic server functionality and add age validation logic

--- Express/app.js
+++ Express/app.js
@@ -1,18 +1,25 @@
 // Create server
 
 import express from "express"
+//import {PORT} from './env.js'
 
 const app = express()
 
 app.get("/" , (req,res) => {
+   //console.log("Directory:", import.meta.dirname);
+   //console.log("File URL:", import.meta.url);
+
+   const __filename = new URL(import.meta.url)
+    
+    
     res.send("Hello ajay")
 })
 
-app.get("/about-page" , (req,res) => {
-    res.send("Hello about page")
-})
+// app.get("/about-page" , (req,res) => {
+//     res.send("Hello about page")
+// })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 
 app.listen(PORT , ()=>{
     console.log(`Port is activated on ${PORT}`);
