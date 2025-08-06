99b8c29e33815f20db61b981760d1ba5c1c8052a
ajay <ajaykumar290904@gmail.com>
2025-08-06 17:28:15 +0530

    style: format code for consistency and readability

--- Express/app.js
+++ Express/app.js
@@ -1,27 +1,29 @@
 // Create server
 
-import express from "express"
-//import {PORT} from './env.js'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get("/" , (req,res) => {
-   //console.log("Directory:", import.meta.dirname);
-   //console.log("File URL:", import.meta.url);
+app.get("/", (req, res) => {
+  //    console.log("Directory:", __dirname);
+  //    console.log("File URL:", __filename);
+  //console.log("Directory:" , import.meta.dirname);
+  //console.log("File url:", import.meta.url);
+  
 
-   const __filename = new URL(import.meta.url)
-    
-    
-    res.send("Hello ajay")
-})
+  const __filename = new URL(import.meta.url);
+  console.log(__filename);
+  
+
+  res.send("Hello ajay");
+});
 
 // app.get("/about-page" , (req,res) => {
 //     res.send("Hello about page")
 // })
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , ()=>{
-    console.log(`Port is activated on ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Port is activated on ${PORT}`);
+});
